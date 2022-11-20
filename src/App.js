1eafeb04ff289f3a80cce5cf6f2c7a541534e1a1
Muhammad Sadewo Wicaksono <114298461+musadewo@users.noreply.github.com>
2022-11-20 17:47:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FormPendataa from "./pages/FormPendataa";
 import ProfilePage from "./pages/ProfilePage";
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 
 function App() {
